b8b490402deed1988c6057497b51957df9c350ef
CNevd <wangyalong20@gmail.com>
2016-05-16 15:08:33 +0800

    add

--- src/difacto/run-difacto.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../tracker/dmlc_local.py -n 1 -s 1 ./build/difacto.dmlc ./guide/demo.conf
---
+++ src/difacto/run_yarn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+../../dmlc-core/tracker/dmlc_yarn.py --vcores 1 -n 2 -s 1 build/difacto.dmlc guide/demo_hdfs.conf
+
